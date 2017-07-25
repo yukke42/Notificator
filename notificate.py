c4d7487d569c32dd6bd889dc461bc96ab5a29c13
yukke42 <muramatsuyusuke9542@gmail.com>
2017-07-25 19:40:00 +0900

    add argment parser

--- notificate.py
+++ notificate.py
@@ -1,69 +1,49 @@
 #!/usr/bin/python3
+import os
 import requests
-import sys, subprocess
+import argparse
+import subprocess
 from configparser import SafeConfigParser
 import logging
-from logging import getLogger,StreamHandler,DEBUG, INFO
-
-DEBUG_MODE = 10
-INFO_MODE  = 20
+from logging import getLogger, DEBUG
 
 class Notificator:
     def __init__(self):
+        path_to_dir = os.path.dirname(os.path.realpath(__file__))
+        logger.debug("path to the directory the file exists." + path_to_dir)
+
         config = SafeConfigParser()
-        config.read('config.ini')
-        self.slack_web_api        = config['slack_config']['slack_web_api']
-        self.channel_name         = config['slack_config']['channel_name']
-        self.user_to_be_mentioned = config['slack_config']['user_to_be_mentioned']
-
-        self.parse_argments()
-        self.execute_the_command()
-        self.post_message()
+        config.read(path_to_dir + '/config.ini')
+        self.slack_web_api = config['slack_config']['slack_web_api']
 
 
-    def parse_argments(self):
+    def execute_command(self, command):
         """
-        引数から実行するコマンド、コマンドの引数を取得
+        @param command(str): got on the command line
         """
-        argc = len(sys.argv)
-        if argc < 2:
-            print("Usage: notificate COMMAND")
-            sys.exit(1)
-
-        self.command  = sys.argv[1]
-        self.argments = " ".join(sys.argv[2:])
-        logger.debug(self.command)
-        logger.debug(self.argments)
-
-
-    def execute_the_command(self):
-        """
-        コマンドを実行し、終了ステータスによりメッセージを作成
-        """
-        cmd = "{cmd} {args}".format(cmd=self.command, args=self.argments)
-        process = subprocess.run(cmd, shell=True, executable='/bin/bash')
-
+        process = subprocess.run(command, shell=True, executable='/bin/bash')
         if process.returncode == 0:
             status_msg = "Done successfully."
         else:
             status_msg = "Error occurred."
 
-        self.msg_to_post = "@{user} {cmd} {msg}".format(
-            user=self.user_to_be_mentioned,
-            cmd=self.command,
+        logger.debug(status_msg)
+        return status_msg
+
+
+    def post_message(self, channel, username, status_msg):
+
+        to_user = "@" + username if username != '' else ''
+
+        msg_to_post = to_user + "\n```{cmd}```\n{msg}".format(
+            cmd=command,
             msg=status_msg
         )
-        logger.debug(self.msg_to_post)
 
-
-    def post_message(self):
-        """
-        メッセージを送信
-        """
         slack_config = {
             'token': self.slack_web_api,
-            'text': self.msg_to_post,
-            'channel': self.channel_name,
+            'text': msg_to_post,
+            'channel': channel,
             'username': 'Notificator',
             'link_names': 1
         }
@@ -72,13 +52,31 @@ class Notificator:
         requests.post(URL, data=slack_config)
 
 
+
 if __name__ == "__main__":
     fmt = "%(levelname)s %(name)s %(funcName)s:%(message)s"
     logging.basicConfig(format=fmt)
     logger = getLogger(__name__)
 
-    # logger.setLevel(DEBUG_MODE)
+    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description='Post a message on slack after the command exits.')
+    parser.add_argument("command", help="enclose command in double quotes")
+    parser.add_argument("--debug", action='store_true', default=False, help="print log to debug")
+    parser.add_argument("-c", "--channel", default='general', help="the destination channel[default: general]")
+    parser.add_argument("-u", "--user", default='', help="the username of receiving a message[default: Nobody]")
+    arg = parser.parse_args()
 
-    logger.debug("DEBUG MODE")
+    command = arg.command
+    channel = arg.channel
+    username = arg.user
+    logger.debug(command)
+    logger.debug(channel)
+    logger.debug(username)
 
-    Notificator()
+    if arg.debug == True:
+        logger.setLevel(DEBUG)
+
+
+    notificator = Notificator()
+    status_msg = notificator.execute_command(command)
+    notificator.post_message(channel, username, status_msg)
